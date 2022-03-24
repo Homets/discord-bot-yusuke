52f40ea1f10bb9e46d7a89fe2c6dc349c7020a96
Homets <b.petitclerc92@gmail.com>
2022-03-24 19:19:07 +0100

    create events and adding the events listener

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fs = require("node:fs");
 const { Client, Collection, Intents } = require("discord.js");
 
 const { token } = require("./config.json");
-const { execute } = require("./commands/user-info");
 
 //create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// Reading commands files
 client.commands = new Collection();
 const commandsFiles = fs
   .readdirSync("./commands")
@@ -18,26 +18,20 @@ for (const file of commandsFiles) {
 
   client.commands.set(command.data.name, command);
 }
-client.once("ready", () => {
-  console.log("ready");
-});
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
+// Reading Events files
+const eventsFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "Cannot execute this commands",
-      ephemeral: true,
-    });
+for (const file of eventsFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 client.login(token, () => {
   console.log("logged in");
