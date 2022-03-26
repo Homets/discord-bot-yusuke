c15ca6ff13c3394e919fdddac87dc1a1583a2cd6
Homets <b.petitclerc92@gmail.com>
2022-03-26 01:15:44 +0100

    clear 100 last messages

--- slash/clear.js
+++ slash/clear.js
@@ -1,11 +1,25 @@
+const Discord = require("discord.js");
+
 module.exports = {
   name: "clear",
   description: "clear some message",
-  async execute(interaction) {
-    console.log(
+
+  async execute(interaction, args) {
+    if (
       interaction.member.roles.cache.some(
         (role) => role.id === "956955044685676584"
       )
-    );
+    ) {
+      interaction.channel
+        .bulkDelete(100)
+        .then((messages) =>
+          console.log(`Bulk deleted ${messages.size} messages`)
+        )
+        .catch(console.error);
+    } else {
+      console.log(
+        `${interaction.user.tag} don't have the permission to do the command`
+      );
+    }
   },
 };
