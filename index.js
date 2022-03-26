e032c61e38c7ad7e6c5a6ebb7c21d5ce212b6e3d
Homets <b.petitclerc92@gmail.com>
2022-03-26 21:21:25 +0100

    adding some cooldown on slash commands

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const { token } = require("./config.json");
 const client = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILDS],
 });
-const { token } = require("./config.json");
 
+// create all collections
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 client.slashCommands = new Discord.Collection();
 
+// Create set for cooldown
+client.command_cooldowns = new Set();
+client.cooldown = 5000;
+// importing all handler files
 ["command_handler", "event_handler"].forEach((handler) => {
   require(`./handlers/${handler}`)(client);
 });
--- slash/server.js
+++ slash/server.js
@@ -2,8 +2,22 @@ module.exports = {
   name: "server",
   description: "Replies with server infos.",
   async execute(interaction) {
-    await interaction.reply(
-      `Server:${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );
+    const client = interaction.client;
+    console.log(client.command_cooldowns.has(interaction.user.id));
+    if (client.command_cooldowns.has(interaction.user.id)) {
+      interaction.reply(
+        "You need to wait the end of the cooldown to do this commands"
+      );
+    } else {
+      await interaction.reply(
+        `Server:${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+      );
+      client.command_cooldowns.add(interaction.user.id);
+      console.log(client.command_cooldowns);
+      setTimeout(() => {
+        client.command_cooldowns.delete(interaction.user.id);
+        console.log(client.command_cooldowns);
+      }, 5000);
+    }
   },
 };
